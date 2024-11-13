218c19034d17bee35bff984a694527afa91a9485
LazeyNg <lazey.ng@gmail.com>
2024-11-13 12:50:59 +0100

    Lab2: Task 2 + 3

--- tda362-labs-main/lab2-textures/lab2_main.cpp
+++ tda362-labs-main/lab2-textures/lab2_main.cpp
@@ -45,7 +45,7 @@ GLuint shaderProgram;
 // the vertex data (in positionBuffer) and color data per vertex (in colorBuffer)
 GLuint positionBuffer, colorBuffer, indexBuffer, vertexArrayObject;
 GLuint texcoordsBuffer;
-
+GLuint texture;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -127,6 +127,19 @@ void initialize()
 	//			Load Texture
 	//************************************
 	// Task 2
+	int w, h, comp;
+	unsigned char* image = stbi_load("../scenes/textures/asphalt.jpg", &w, &h, &comp, STBI_rgb_alpha);
+	
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	free(image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
 
@@ -169,6 +182,8 @@ void display(void)
 	glUniform3f(loc, camera_pan, 10, 0);
 
 	// Task 3.1
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
 	glBindVertexArray(vertexArrayObject);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
--- tda362-labs-main/lab2-textures/simple.frag
+++ tda362-labs-main/lab2-textures/simple.frag
@@ -7,7 +7,7 @@ precision highp float;
 in vec2 texCoord;
 
 // Task 3.4: Receive the texture as a uniform
-
+layout(binding = 0) uniform sampler2D colortexture;
 layout(location = 0) out vec4 fragmentColor;
 
 void main()
@@ -15,5 +15,6 @@ void main()
 	// Task 1: Use the texture coordinates for the x,y of the color output
 	// Task 3.5: Sample the texture with the texture coordinates and use that for the color
 	// fragmentColor = vec4(1.0, 1.0, 1.0, 1.0);
-	fragmentColor = vec4(texCoord.x, texCoord.y, 0.0, 0.0); 
+	// fragmentColor = vec4(texCoord.x, texCoord.y, 0.0, 0.0); 
+	fragmentColor = texture2D(colortexture, texCoord.xy);
 }
