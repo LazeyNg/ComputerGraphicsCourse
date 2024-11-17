e1f3c44db12b1520ec827663e243ce1ec6aa0e06
LazeyNg <lazey.ng@gmail.com>
2024-11-17 15:10:31 +0100

    Lab 2: Fix Anisotrophy

--- tda362-labs-main/lab2-textures/lab2_main.cpp
+++ tda362-labs-main/lab2-textures/lab2_main.cpp
@@ -329,6 +329,8 @@ void gui()
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
 	            ImGui::GetIO().Framerate);
 	// ----------------------------------------------------------
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisotropy);
+
 
 }
 
